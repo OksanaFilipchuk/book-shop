ad8f1206106d678a13581f43a5391734fad8f00c
OksanaFilipchuk <filipchuk.oksana@gmail.com>
2022-12-14 01:04:42 +0200

    fix: format files

--- form-page/script.js
+++ form-page/script.js
@@ -1,110 +1,137 @@
-function focusHandler (event){
-	if(event.target.classList.contains("input-invalid")){
-		event.target.classList.remove("input-invalid");
-		event.target.nextElementSibling.innerHTML = "";
-	}
+function focusHandler(event) {
+  if (event.target.classList.contains("input-invalid")) {
+    event.target.classList.remove("input-invalid");
+    event.target.nextElementSibling.innerHTML = "";
+  }
 }
 
-function getInvalidStyles(event){
-	event.target.classList.add("input-invalid");
-	event.target.nextElementSibling.innerHTML = "The field is invalid";
+function getInvalidStyles(event) {
+  event.target.classList.add("input-invalid");
+  event.target.nextElementSibling.innerHTML = "The field is invalid";
 }
 
-function checkFormValidation(){
-	if(fields.every(el => el.validation === true)){
-		document.querySelector("input[type = submit]").removeAttribute("disabled");
-	} else document.querySelector("input[type=submit]").setAttribute("disabled", "disabled");
+function checkFormValidation() {
+  if (fields.every((el) => el.validation === true)) {
+    document.querySelector("input[type = submit]").removeAttribute("disabled");
+  } else
+    document
+      .querySelector("input[type=submit]")
+      .setAttribute("disabled", "disabled");
 }
 
 let fields = [
-	{
-		id: "#name",
-		validation: false,
-		validCondision: function(event){
-			return event.target.value.length<4 || !(/^[a-zA-Z()]+$/).test(event.target.value)
-		},
-	},
-	{
-		id: "#surname",
-		validation: false,
-		validCondision: function(event){
-			return event.target.value.length<5 || !(/^[a-zA-Z()]+$/).test(event.target.value)
-		},
-	},
-	{
-		id: "#delivery-date",
-		validation: false,
-		validCondision: function(event){
-			let today = new Date();
-			let tommorow = new Date(today.getTime() - today.getHours()*60*60*1000 - today.getMinutes()*60*1000 - today.getSeconds()*1000 + 24*60*60*1000);
-			return new Date(event.target.value) < tommorow
-		},
-	},
-	{
-		id: "#street",
-		validation: false,
-		validCondision: function(event){
-			return event.target.value.length<5 || !(/^[a-zA-Z()0-9]+$/).test(event.target.value)
-		}
-	},
-	{
-		id: "#house-number",
-		validation: false,
-		validCondision: function(event){
-			return !(/^[0-9]+$/).test(event.target.value)
-		}
-	},
-	{
-		id: "#flat-number",
-		validation: false,
-		validCondision: function(event){
-			return !(/^[0-9]+\x2d?[0-9]*$/).test(event.target.value)
-		}
-	},
-]
+  {
+    id: "#name",
+    validation: false,
+    validCondision: function (event) {
+      return (
+        event.target.value.length < 4 ||
+        !/^[a-zA-Z()]+$/.test(event.target.value)
+      );
+    },
+  },
+  {
+    id: "#surname",
+    validation: false,
+    validCondision: function (event) {
+      return (
+        event.target.value.length < 5 ||
+        !/^[a-zA-Z()]+$/.test(event.target.value)
+      );
+    },
+  },
+  {
+    id: "#delivery-date",
+    validation: false,
+    validCondision: function (event) {
+      let today = new Date();
+      let tommorow = new Date(
+        today.getTime() -
+          today.getHours() * 60 * 60 * 1000 -
+          today.getMinutes() * 60 * 1000 -
+          today.getSeconds() * 1000 +
+          24 * 60 * 60 * 1000
+      );
+      return new Date(event.target.value) < tommorow;
+    },
+  },
+  {
+    id: "#street",
+    validation: false,
+    validCondision: function (event) {
+      return (
+        event.target.value.length < 5 ||
+        !/^[a-zA-Z()0-9]+$/.test(event.target.value)
+      );
+    },
+  },
+  {
+    id: "#house-number",
+    validation: false,
+    validCondision: function (event) {
+      return !/^[0-9]+$/.test(event.target.value);
+    },
+  },
+  {
+    id: "#flat-number",
+    validation: false,
+    validCondision: function (event) {
+      return !/^[0-9]+\x2d?[0-9]*$/.test(event.target.value);
+    },
+  },
+];
 
-fields.forEach(elem => {
-	document.querySelector(elem.id).addEventListener("focus", focusHandler);
-	document.querySelector(elem.id).addEventListener("blur", (event) => {
-		if(elem.validCondision(event)){
-			getInvalidStyles(event);
-			elem.validation = false;
-		}else {
-			elem.validation = true;
-		}
-		checkFormValidation();
-	})
-})
+fields.forEach((elem) => {
+  document.querySelector(elem.id).addEventListener("focus", focusHandler);
+  document.querySelector(elem.id).addEventListener("blur", (event) => {
+    if (elem.validCondision(event)) {
+      getInvalidStyles(event);
+      elem.validation = false;
+    } else {
+      elem.validation = true;
+    }
+    checkFormValidation();
+  });
+});
 
 //only 2 gifts available to choose
 let giftInputs = document.querySelectorAll("input[name = gift]");
-giftInputs.forEach(el => el.addEventListener("change", ()=>{
-	let checkedGiftValue = Array.from(giftInputs).reduce((ac,curr) =>ac+(curr.checked?1:0), 0)
-	if(checkedGiftValue === 2){
-		Array.from(giftInputs).forEach(el => {if(!el.checked)el.setAttribute("disabled", "disabled")})
-	}else{
-		Array.from(giftInputs).forEach(el => {if(!el.checked)el.removeAttribute("disabled")})
-	}
-}))
+giftInputs.forEach((el) =>
+  el.addEventListener("change", () => {
+    let checkedGiftValue = Array.from(giftInputs).reduce(
+      (ac, curr) => ac + (curr.checked ? 1 : 0),
+      0
+    );
+    if (checkedGiftValue === 2) {
+      Array.from(giftInputs).forEach((el) => {
+        if (!el.checked) el.setAttribute("disabled", "disabled");
+      });
+    } else {
+      Array.from(giftInputs).forEach((el) => {
+        if (!el.checked) el.removeAttribute("disabled");
+      });
+    }
+  })
+);
 
-document.querySelector(".form").addEventListener("submit", (e)=>{
-	e.preventDefault();
-	let customerInformation = document.querySelector(".customer-info");
-	let wrapper = document.createElement("div");
-	wrapper.className = "wrapper";
-	wrapper.innerHTML = "<p class = 'customer-info-header'>Summarized information</p>";
-	fields.forEach( el =>{
-		let p = document.createElement("p");
-		p.textContent = el.id.slice(1) +": ";
-		let span = document.createElement("span");
-		span.className = "span";
-		span.textContent = document.querySelector(el.id).value;
-		p.appendChild(span)
-		p.className = "customer-info-item";
-		wrapper.appendChild(p)
-	})	
-	customerInformation.appendChild(wrapper)
-	customerInformation.classList.add("customer-info-active");
-	document.querySelector("body").classList.add("body-hidden")
-})
-
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let customerInformation = document.querySelector(".customer-info");
+  let wrapper = document.createElement("div");
+  wrapper.className = "wrapper";
+  wrapper.innerHTML =
+    "<p class = 'customer-info-header'>Summarized information</p>";
+  fields.forEach((el) => {
+    let p = document.createElement("p");
+    p.textContent = el.id.slice(1) + ": ";
+    let span = document.createElement("span");
+    span.className = "span";
+    span.textContent = document.querySelector(el.id).value;
+    p.appendChild(span);
+    p.className = "customer-info-item";
+    wrapper.appendChild(p);
+  });
+  customerInformation.appendChild(wrapper);
+  customerInformation.classList.add("customer-info-active");
+  document.querySelector("body").classList.add("body-hidden");
+});
--- main-page/script.js
+++ main-page/script.js
@@ -1,134 +1,135 @@
 let cart = [];
 let cartSum = 0;
 
-function showPageContent(data){
+function showPageContent(data) {
+  function showCart() {
+    if (cartMenuBackground.classList.contains("cart-menu-active")) {
+      cartMenuBackground.classList.remove("cart-menu-active");
+      document.querySelector("body").classList.remove("body-active-menu");
+      cartWrapper.classList.remove("cart-wrapper-active");
+      confirmOrderButton.classList.remove("confirm-order-button-active");
+    } else {
+      cartMenuBackground.classList.add("cart-menu-active");
+      document.querySelector("body").classList.add("body-active-menu");
+      cartWrapper.classList.add("cart-wrapper-active");
+      confirmOrderButton.classList.add("confirm-order-button-active");
+      cartMenuWrapper.innerHTML = null;
+      displayCartContent();
+    }
+  }
 
-	function showCart(){
-		if(cartMenuBackground.classList.contains("cart-menu-active")){
-			cartMenuBackground.classList.remove("cart-menu-active");
-			document.querySelector("body").classList.remove("body-active-menu");
-			cartWrapper.classList.remove("cart-wrapper-active");
-			confirmOrderButton.classList.remove("confirm-order-button-active")
-		} else {
-			cartMenuBackground.classList.add("cart-menu-active");
-			document.querySelector("body").classList.add("body-active-menu");		
-			cartWrapper.classList.add("cart-wrapper-active");
-			confirmOrderButton.classList.add("confirm-order-button-active")
-			cartMenuWrapper.innerHTML = null;
-			displayCartContent()
-		}
-	}
-
-	function displayCartContent(){
-		if(cart.length>0){
-			cart.forEach(el => {
-				const bookBlock = document.createElement("div");
-				bookBlock.classList = "cart-book-block";
-				bookBlock.innerHTML = `<img src = "${el.imageLink}" alt = "${el.title}" class = "cart-book-img">
+  function displayCartContent() {
+    if (cart.length > 0) {
+      cart.forEach((el) => {
+        const bookBlock = document.createElement("div");
+        bookBlock.classList = "cart-book-block";
+        bookBlock.innerHTML = `<img src = "${el.imageLink}" alt = "${
+          el.title
+        }" class = "cart-book-img">
 															<h4 class = "cart-book-title">${el.title}</h4>
 															<h5 class = "cart-book-author">${el.author}</h5>
 															<span class = "cart-book-price">${el.price}$</span>
 															<span class = "cart-book-number">${el.number}</span>
-															<span class = "cart-book-sum">${el.number*el.price}$</span>`;
-				let removeBook = document.createElement("img");
-				removeBook.setAttribute("src", "./images/trash.svg");
-				removeBook.setAttribute("alt", "remove");
-				removeBook.classList.add("remove-book")
-				removeBook.addEventListener("click", ()=>{
-					cart.splice(cart.indexOf(el),1);
-					cartMenuWrapper.innerHTML= null;
-					displayCartContent();
-					updateCartSum();
-				})
-				bookBlock.appendChild(removeBook)
-				cartMenuWrapper.appendChild(bookBlock)	
-			})
-			confirmOrderButton.disabled= false;
-		} else {
-			let message = document.createElement("p");
-			message.textContent = "Your cart is empty(";
-			message.classList = "message"
-			cartMenuWrapper.appendChild(message);
-			confirmOrderButton.disabled = true;
-		}	
+															<span class = "cart-book-sum">${el.number * el.price}$</span>`;
+        let removeBook = document.createElement("img");
+        removeBook.setAttribute("src", "./images/trash.svg");
+        removeBook.setAttribute("alt", "remove");
+        removeBook.classList.add("remove-book");
+        removeBook.addEventListener("click", () => {
+          cart.splice(cart.indexOf(el), 1);
+          cartMenuWrapper.innerHTML = null;
+          displayCartContent();
+          updateCartSum();
+        });
+        bookBlock.appendChild(removeBook);
+        cartMenuWrapper.appendChild(bookBlock);
+      });
+      confirmOrderButton.disabled = false;
+    } else {
+      let message = document.createElement("p");
+      message.textContent = "Your cart is empty(";
+      message.classList = "message";
+      cartMenuWrapper.appendChild(message);
+      confirmOrderButton.disabled = true;
+    }
+  }
 
-	}
-	
-	let root = document.querySelector(".root");
-	let headerFragment = document.createDocumentFragment();
-	let header = document.createElement("header");
-	let headerContainer = document.createElement("div");
-	headerContainer.className = "container";
-	headerContainer.classList.add("header-container")
-	header.appendChild(headerContainer);
-	headerFragment.appendChild(header);
-	headerContainer.innerHTML = `<div>
+  let root = document.querySelector(".root");
+  let headerFragment = document.createDocumentFragment();
+  let header = document.createElement("header");
+  let headerContainer = document.createElement("div");
+  headerContainer.className = "container";
+  headerContainer.classList.add("header-container");
+  header.appendChild(headerContainer);
+  headerFragment.appendChild(header);
+  headerContainer.innerHTML = `<div>
 																<h1>Book shop</h1>
 																<q class = "quot">Live as if you were to die tomorrow. Learn as if you were to live forever</q>
 																<p class = "author">Mahatma Gandhi</p>
 															</div>`;
-	let cartWrapper = document.createElement("div");
-	let shoppingBag = document.createElement("img");
-	shoppingBag.className = "shopping-bag";
-	shoppingBag.setAttribute("src", "./images/shopping-bag.svg");
-	shoppingBag.setAttribute("alt", "cart");
-	shoppingBag.addEventListener("click", showCart);
-	cartWrapper.appendChild(shoppingBag);
-	cartWrapper.addEventListener("dragover", (event)=> event.preventDefault())
-	cartWrapper.addEventListener("drop", (event)=> {
-		let el = event.dataTransfer.getData("elData");
-		if(cart.includes(data[el])){
-			cart[cart.indexOf(data[el])].number = cart[cart.indexOf(data[el])].number + 1;				
-		}else {
-			data[el].number = 1;
-			cart.push(data[el]);
-		}
-		updateCartSum()
-	})
+  let cartWrapper = document.createElement("div");
+  let shoppingBag = document.createElement("img");
+  shoppingBag.className = "shopping-bag";
+  shoppingBag.setAttribute("src", "./images/shopping-bag.svg");
+  shoppingBag.setAttribute("alt", "cart");
+  shoppingBag.addEventListener("click", showCart);
+  cartWrapper.appendChild(shoppingBag);
+  cartWrapper.addEventListener("dragover", (event) => event.preventDefault());
+  cartWrapper.addEventListener("drop", (event) => {
+    let el = event.dataTransfer.getData("elData");
+    if (cart.includes(data[el])) {
+      cart[cart.indexOf(data[el])].number =
+        cart[cart.indexOf(data[el])].number + 1;
+    } else {
+      data[el].number = 1;
+      cart.push(data[el]);
+    }
+    updateCartSum();
+  });
 
-	let shoppingBagCounter = document.createElement("div");
-	shoppingBagCounter.className = "shopping-bag-counter";
-	shoppingBagCounter.textContent= "0$";
-	cartWrapper.appendChild(shoppingBagCounter);
-	cartWrapper.className = "cart-wrapper"
-	
-	headerContainer.appendChild(cartWrapper);
-	let confirmOrderButton = document.createElement("button");
-	let buttonLink = document.createElement("a");
-	buttonLink.className = "button-link";
-	buttonLink.setAttribute("href", "../form-page/index.html");
-	buttonLink.appendChild(confirmOrderButton);
-	confirmOrderButton.className = "confirm-order-button";
-	confirmOrderButton.textContent = "Confirm order";
-	cartWrapper.appendChild(buttonLink);
-	let cartMenuBackground = document.createElement("div");
-	cartMenuBackground.className = "cart-menu";
-	let cartMenuWrapper = document.createElement("div");
-	cartMenuWrapper.className = "cart-menu-wrapper";
-	cartMenuBackground.appendChild(cartMenuWrapper);
-	header.appendChild(cartMenuBackground);
+  let shoppingBagCounter = document.createElement("div");
+  shoppingBagCounter.className = "shopping-bag-counter";
+  shoppingBagCounter.textContent = "0$";
+  cartWrapper.appendChild(shoppingBagCounter);
+  cartWrapper.className = "cart-wrapper";
 
-	let mainFragment = document.createDocumentFragment();
-	let main = document.createElement("main");
-	let mainContainer = document.createElement("div");
-	mainContainer.className = "container";
-	let catalogWrapper = document.createElement("div");
-	catalogWrapper.className = "catalog-wrapper";
-	let catalogHeader = document.createElement("h3")
-	catalogHeader.textContent = "Catalog";
-	catalogHeader.className = "catalog-header";
-	let catalog = document.createElement("div");
-	catalog.className = "catalog";
-	catalogWrapper.appendChild(catalogHeader);
-	catalogWrapper.appendChild(catalog);
-	main.appendChild(mainContainer);
-	mainContainer.appendChild(catalogWrapper);
-	mainFragment.appendChild(main)
+  headerContainer.appendChild(cartWrapper);
+  let confirmOrderButton = document.createElement("button");
+  let buttonLink = document.createElement("a");
+  buttonLink.className = "button-link";
+  buttonLink.setAttribute("href", "../form-page/index.html");
+  buttonLink.appendChild(confirmOrderButton);
+  confirmOrderButton.className = "confirm-order-button";
+  confirmOrderButton.textContent = "Confirm order";
+  cartWrapper.appendChild(buttonLink);
+  let cartMenuBackground = document.createElement("div");
+  cartMenuBackground.className = "cart-menu";
+  let cartMenuWrapper = document.createElement("div");
+  cartMenuWrapper.className = "cart-menu-wrapper";
+  cartMenuBackground.appendChild(cartMenuWrapper);
+  header.appendChild(cartMenuBackground);
 
-	let footerFragment = document.createDocumentFragment();
-	let footer = document.createElement("footer");
-	footerFragment.appendChild(footer)
-	footer.innerHTML = `<div class = "container footer-container">
+  let mainFragment = document.createDocumentFragment();
+  let main = document.createElement("main");
+  let mainContainer = document.createElement("div");
+  mainContainer.className = "container";
+  let catalogWrapper = document.createElement("div");
+  catalogWrapper.className = "catalog-wrapper";
+  let catalogHeader = document.createElement("h3");
+  catalogHeader.textContent = "Catalog";
+  catalogHeader.className = "catalog-header";
+  let catalog = document.createElement("div");
+  catalog.className = "catalog";
+  catalogWrapper.appendChild(catalogHeader);
+  catalogWrapper.appendChild(catalog);
+  main.appendChild(mainContainer);
+  mainContainer.appendChild(catalogWrapper);
+  mainFragment.appendChild(main);
+
+  let footerFragment = document.createDocumentFragment();
+  let footer = document.createElement("footer");
+  footerFragment.appendChild(footer);
+  footer.innerHTML = `<div class = "container footer-container">
 												<ul class = "social-media">
 													<li class = "social-media-item">
 														<img src="./images/facebook.svg" class="social-media-img" alt="Facebook">
@@ -150,99 +151,96 @@ function showPageContent(data){
 													<li class="nav-list-item"><a href="#" class="nav-list-item-link">Contact us</a></li>
 												</ul>
 											</nav>
-											</div>`
+											</div>`;
 
-	root.appendChild(headerFragment);
-	root.appendChild(mainFragment);
-	root.appendChild(footerFragment);
-	showBooks(data, catalog);
+  root.appendChild(headerFragment);
+  root.appendChild(mainFragment);
+  root.appendChild(footerFragment);
+  showBooks(data, catalog);
 }
 
-function updateCartSum(){
-	if(cart.length === 0){
-		cartSum = 0;
-	} else if(cart.length===1){
-		cartSum = cart[0].price *cart[0].number;
-	}else {
-		cartSum = cart.reduce((accum,current)=>	accum + current.price * current.number,
-		0)
-	}
-	document.querySelector(".shopping-bag-counter").textContent = cartSum +" $";
+function updateCartSum() {
+  if (cart.length === 0) {
+    cartSum = 0;
+  } else if (cart.length === 1) {
+    cartSum = cart[0].price * cart[0].number;
+  } else {
+    cartSum = cart.reduce(
+      (accum, current) => accum + current.price * current.number,
+      0
+    );
+  }
+  document.querySelector(".shopping-bag-counter").textContent = cartSum + " $";
 }
 
-function showBooks(data){
-	data.forEach((el, index)=>{
+function showBooks(data) {
+  data.forEach((el, index) => {
+    function showPopUp() {
+      popUpBackground.classList.add("pop-up-background-active");
+      document.querySelector("body").classList.add("body-active-menu");
+    }
 
-		function showPopUp(){
-			popUpBackground.classList.add("pop-up-background-active");
-			document.querySelector("body").classList.add("body-active-menu");
-		}
+    function closePopUp() {
+      popUpBackground.classList.remove("pop-up-background-active");
+      document.querySelector("body").classList.remove("body-active-menu");
+    }
 
-		function closePopUp (){
-			popUpBackground.classList.remove("pop-up-background-active");
-			document.querySelector("body").classList.remove("body-active-menu");	
-		}
-
-		const bookBlock = document.createElement("div");
-		bookBlock.classList = "book-block";
-		bookBlock.innerHTML = `<img src = "${el.imageLink}" alt = "${el.title}" class = "book-img">
+    const bookBlock = document.createElement("div");
+    bookBlock.classList = "book-block";
+    bookBlock.innerHTML = `<img src = "${el.imageLink}" alt = "${el.title}" class = "book-img">
 													<h4 class = "book-title">${el.title}</h4>
 													<h5 class = "book-author">${el.author}</h5>
 													`;
-		let readMore = document.createElement("span");
-		readMore.className = "read-more";
-		readMore.textContent = "Read more";
-		bookBlock.appendChild(readMore);			
-		let popUpBackground = document.createElement("div");
-		popUpBackground.className = "pop-up-background";
-		let popUpBlock = document.createElement("div");
-		popUpBlock.className = "pop-up-block";
-		popUpBlock.textContent = el.description;
-		let closePopUpButton = document.createElement("p");
-		closePopUpButton .className = "close-pop-up";
-		closePopUpButton .textContent = "x";
-		closePopUpButton .addEventListener("click", closePopUp)
-		popUpBackground.appendChild(popUpBlock);
-		popUpBlock.appendChild(closePopUpButton );
-		bookBlock.appendChild(popUpBackground);
-		readMore.addEventListener("click", showPopUp);
+    let readMore = document.createElement("span");
+    readMore.className = "read-more";
+    readMore.textContent = "Read more";
+    bookBlock.appendChild(readMore);
+    let popUpBackground = document.createElement("div");
+    popUpBackground.className = "pop-up-background";
+    let popUpBlock = document.createElement("div");
+    popUpBlock.className = "pop-up-block";
+    popUpBlock.textContent = el.description;
+    let closePopUpButton = document.createElement("p");
+    closePopUpButton.className = "close-pop-up";
+    closePopUpButton.textContent = "x";
+    closePopUpButton.addEventListener("click", closePopUp);
+    popUpBackground.appendChild(popUpBlock);
+    popUpBlock.appendChild(closePopUpButton);
+    bookBlock.appendChild(popUpBackground);
+    readMore.addEventListener("click", showPopUp);
 
-		bookBlock.setAttribute("id", `book-block${index}`);
-		bookBlock.setAttribute("draggable", "true");
-		bookBlock.addEventListener("dragstart", (ev)=> {
+    bookBlock.setAttribute("id", `book-block${index}`);
+    bookBlock.setAttribute("draggable", "true");
+    bookBlock.addEventListener("dragstart", (ev) => {
+      ev.dataTransfer.setData("elData", index);
+    });
 
-			ev.dataTransfer.setData("elData", index)
-		})
+    let bookPrice = document.createElement("span");
+    bookPrice.className = "book-price";
+    bookPrice.textContent = el.price + "$";
+    bookBlock.appendChild(bookPrice);
+    let buyButton = document.createElement("button");
+    buyButton.className = "buy-button";
+    buyButton.textContent = "Buy book";
+    bookBlock.appendChild(buyButton);
+    buyButton.addEventListener("click", () => {
+      if (cart.includes(el)) {
+        cart[cart.indexOf(el)].number = cart[cart.indexOf(el)].number + 1;
+      } else {
+        el.number = 1;
+        cart.push(el);
+      }
+      updateCartSum();
+    });
 
-		let bookPrice = document.createElement("span");
-		bookPrice.className = "book-price";
-		bookPrice.textContent = el.price+"$";
-		bookBlock.appendChild(bookPrice);
-		let buyButton = document.createElement("button");
-		buyButton.className = "buy-button";
-		buyButton.textContent = "Buy book";
-		bookBlock.appendChild(buyButton);
-		buyButton.addEventListener("click", ()=>{
-
-			if(cart.includes(el)){
-				cart[cart.indexOf(el)].number = cart[cart.indexOf(el)].number + 1;				
-			}else {
-				el.number = 1;
-				cart.push(el);
-			}
-			updateCartSum()
-		})
-
-		document.querySelector(".catalog").appendChild(bookBlock);	
-	})
-
+    document.querySelector(".catalog").appendChild(bookBlock);
+  });
 }
 
-fetch('books.json')
-.then(response => {
-	return response.json();
-})
-.then(data => {
-	showPageContent(data);
-})
-
+fetch("books.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    showPageContent(data);
+  });
